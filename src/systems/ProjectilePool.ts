94013e18f9ab00576ed4ab73b3ce8290f64f0189
7015725 <62368672+7015725@users.noreply.github.com>
2026-08-11 16:17:55 +0800

    perf: track active projectiles only

--- src/systems/ProjectilePool.ts
+++ src/systems/ProjectilePool.ts
@@ -3,11 +3,17 @@ import type { DamageContext, Targetable } from '../combat/types';
 import { Projectile } from '../entities/Projectile';
 
 export class ProjectilePool {
-  private readonly projectiles: Projectile[];
+  private readonly projectiles: Projectile[] = [];
+  private readonly freeProjectiles: Projectile[] = [];
+  private readonly activeProjectiles: Projectile[] = [];
   private activeCountValue = 0;
 
   constructor(private readonly scene: Phaser.Scene, initialSize = 256) {
-    this.projectiles = Array.from({ length: initialSize }, () => new Projectile(scene));
+    for (let index = 0; index < initialSize; index += 1) {
+      const projectile = new Projectile(scene);
+      this.projectiles.push(projectile);
+      this.freeProjectiles.push(projectile);
+    }
   }
 
   get activeCount(): number {
@@ -25,25 +31,38 @@ export class ProjectilePool {
     speed: number,
     damageContext: DamageContext,
   ): void {
-    let projectile = this.projectiles.find((candidate) => !candidate.inUse);
+    let projectile = this.freeProjectiles.pop();
     if (!projectile) {
       projectile = new Projectile(this.scene);
       this.projectiles.push(projectile);
     }
     projectile.fire(x, y, target, speed, damageContext);
+    this.activeProjectiles.push(projectile);
+    this.activeCountValue = this.activeProjectiles.length;
   }
 
   update(deltaMs: number): void {
-    let active = 0;
-    for (const projectile of this.projectiles) {
+    let writeIndex = 0;
+    for (let index = 0; index < this.activeProjectiles.length; index += 1) {
+      const projectile = this.activeProjectiles[index];
       projectile.update(deltaMs);
-      if (projectile.inUse) active += 1;
+      if (projectile.inUse) {
+        this.activeProjectiles[writeIndex] = projectile;
+        writeIndex += 1;
+      } else {
+        this.freeProjectiles.push(projectile);
+      }
     }
-    this.activeCountValue = active;
+    this.activeProjectiles.length = writeIndex;
+    this.activeCountValue = writeIndex;
   }
 
   clear(): void {
-    for (const projectile of this.projectiles) projectile.release();
+    for (const projectile of this.activeProjectiles) {
+      projectile.release();
+      this.freeProjectiles.push(projectile);
+    }
+    this.activeProjectiles.length = 0;
     this.activeCountValue = 0;
   }
 }
