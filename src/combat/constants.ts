fd6a294bfdcebc6c3abb14f4f67df0ee52535155
7015725 <62368672+7015725@users.noreply.github.com>
2026-08-11 12:00:49 +0800

    Add flying enemy and M0.6 wave data

--- src/combat/constants.ts
+++ src/combat/constants.ts
@@ -10,20 +10,32 @@ export const BASE_Y = 1490;
 export const TURRET_Y = 1435;
 
 export const LANE_OFFSETS = [-160, -80, 0, 80, 160] as const;
+export const AIR_LANE_OFFSETS = [-260, -130, 0, 130, 260] as const;
 
 export const WAVE_DURATION_MS = 30_000;
 export const WAVE_SPAWN_WINDOW_MS = 24_000;
+export const WAVE20_AIR_ESCORT_COUNT = 6;
 
 export const TEST_WAVE_COMPOSITIONS = [
-  { infantry: 8, heavy: 0 },
-  { infantry: 9, heavy: 0 },
-  { infantry: 11, heavy: 0 },
-  { infantry: 13, heavy: 0 },
-  { infantry: 15, heavy: 0 },
-  { infantry: 14, heavy: 1 },
-  { infantry: 15, heavy: 2 },
-  { infantry: 16, heavy: 2 },
-  { infantry: 17, heavy: 3 },
+  { infantry: 8, heavy: 0, flying: 0 },
+  { infantry: 9, heavy: 0, flying: 0 },
+  { infantry: 11, heavy: 0, flying: 0 },
+  { infantry: 13, heavy: 0, flying: 0 },
+  { infantry: 15, heavy: 0, flying: 0 },
+  { infantry: 14, heavy: 1, flying: 0 },
+  { infantry: 15, heavy: 2, flying: 0 },
+  { infantry: 16, heavy: 2, flying: 0 },
+  { infantry: 17, heavy: 3, flying: 0 },
+  null,
+  { infantry: 18, heavy: 2, flying: 0 },
+  { infantry: 19, heavy: 2, flying: 0 },
+  { infantry: 20, heavy: 3, flying: 0 },
+  { infantry: 21, heavy: 3, flying: 0 },
+  { infantry: 22, heavy: 3, flying: 0 },
+  { infantry: 22, heavy: 4, flying: 0 },
+  { infantry: 23, heavy: 4, flying: 0 },
+  { infantry: 24, heavy: 4, flying: 0 },
+  { infantry: 25, heavy: 5, flying: 0 },
 ] as const;
 
 export const BASE_MAX_HP = 1000;
@@ -32,6 +44,7 @@ export const INFANTRY: EnemyDefinition = {
   hp: 80,
   armor: 0,
   armorGrade: 'UNARMORED',
+  domain: 'GROUND',
   moveSpeed: 52,
   attackDamage: 12,
   attackIntervalMs: 1200,
@@ -45,6 +58,7 @@ export const HEAVY: EnemyDefinition = {
   hp: 220,
   armor: 100,
   armorGrade: 'HEAVY',
+  domain: 'GROUND',
   moveSpeed: 36,
   attackDamage: 22,
   attackIntervalMs: 1500,
@@ -54,10 +68,25 @@ export const HEAVY: EnemyDefinition = {
   color: 0x475569,
 };
 
+export const FLYING: EnemyDefinition = {
+  hp: 60,
+  armor: 0,
+  armorGrade: 'UNARMORED',
+  domain: 'AIR',
+  moveSpeed: 90,
+  attackDamage: 8,
+  attackIntervalMs: 800,
+  xp: 7,
+  credits: 3,
+  size: 38,
+  color: 0x38bdf8,
+};
+
 export const BOSS: EnemyDefinition = {
   hp: 1800,
   armor: 20,
   armorGrade: 'LIGHT',
+  domain: 'GROUND',
   moveSpeed: 50,
   attackDamage: 32,
   attackIntervalMs: 1500,
@@ -72,6 +101,7 @@ export const AUTO_CANNON: WeaponDefinition = {
   name: 'Auto Cannon',
   mode: 'projectile',
   targetingRule: 'frontmost',
+  targetDomains: ['GROUND', 'AIR'],
   damage: 18,
   attackIntervalMs: 500,
   range: 720,
@@ -88,6 +118,7 @@ export const LMG: WeaponDefinition = {
   name: 'LMG Nest',
   mode: 'projectile',
   targetingRule: 'frontmost',
+  targetDomains: ['GROUND', 'AIR'],
   damage: 8,
   attackIntervalMs: 100,
   range: 620,
@@ -104,6 +135,7 @@ export const SHOTGUN: WeaponDefinition = {
   name: 'Tac-Shotgun Bunker',
   mode: 'shotgun',
   targetingRule: 'frontmost',
+  targetDomains: ['GROUND'],
   damage: 5,
   attackIntervalMs: 1050,
   range: 430,
@@ -122,6 +154,7 @@ export const SNIPER: WeaponDefinition = {
   name: 'Bolt-Action Sniper',
   mode: 'projectile',
   targetingRule: 'highest-hp',
+  targetDomains: ['GROUND', 'AIR'],
   damage: 100,
   attackIntervalMs: 2300,
   range: 1500,
@@ -138,6 +171,7 @@ export const AUTO_GL: WeaponDefinition = {
   name: 'Auto-GL',
   mode: 'grenade',
   targetingRule: 'frontmost',
+  targetDomains: ['GROUND'],
   damage: 55,
   attackIntervalMs: 1350,
   range: 900,
@@ -156,6 +190,7 @@ export const TESLA: WeaponDefinition = {
   name: 'Tesla Coil',
   mode: 'tesla',
   targetingRule: 'frontmost',
+  targetDomains: ['GROUND'],
   damage: 20,
   attackIntervalMs: 1600,
   range: 520,
